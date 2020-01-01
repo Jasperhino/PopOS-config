f29b0c59ab1719b459dd514d8316360395c32fc2
Jasper <blum.jasper@student.hpi.de>
2020-01-01 20:50:13 +0100

    Update config.bash

--- config.bash
+++ config.bash
@@ -1,11 +1,10 @@
 ## This Script is My Personal Quick Install for Ubuntu/Mint based systems configs
-##
 
 git clone --bare https://github.com/Jasperhino/dotfiles.git $HOME/.dotfiles
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 dotfiles checkout
 
-dconf load / < .dconf
+dconf load / < $HOME/.dconf
 
 gsettings set org.gnome.desktop.background picture-uri "file:///home/$USER/Pictures/Wallpapers/wallpaper.png"
 
